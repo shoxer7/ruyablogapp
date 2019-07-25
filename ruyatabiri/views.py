62db87e7a90f1408c80e029d5e480bdd3a2e64ae
shoxer7 <35742291+shoxer7@users.noreply.github.com>
2019-07-25 17:56:07 +0300

    Update views.py

--- ruyatabiri/views.py
+++ ruyatabiri/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render,get_object_or_404
 from .models import RuyaTabiri
+from django.contrib.sitemaps import Sitemap
 # Create your views here.
+
+class BlogSitemap(Sitemap): 
+    changefreq = "daily"
+    priority = 1.0
+    def items(self):
+        return RuyaTabiri.objects.all()
+    def lastmod(self,obj):
+        return obj.created_date
+    def location(self,obj):
+        return obj.title
+
 
 def index(request):
     keyword = request.GET.get("keyword")
@@ -10,12 +22,12 @@ def index(request):
     return render(request,"index.html")
 
 def ruyatabiridetay(request,id):
-    template_name = "ruyatabiridetay.html
+    template_name = "ruyatabiridetay.html"
     ruyatabiri = get_object_or_404(RuyaTabiri,id = id) 
     return render(request, template_name, {"ruyatabiri":ruyatabiri})
 
 def ruya_tabiri_liste(request, char):
-    template_name = "ruya_tabirleri_liste.html
+    template_name = "ruya_tabirleri_liste.html"
     keyword = request.GET.get("keyword")
     if keyword:
         ruyatabirleri = RuyaTabiri.objects.filter(title__contains = keyword)
