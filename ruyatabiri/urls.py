0150d47fd62da76faacf967e4c88bcab3cca634f
shoxer7 <35742291+shoxer7@users.noreply.github.com>
2019-07-25 17:55:23 +0300

    Update urls.py

--- ruyatabiri/urls.py
+++ ruyatabiri/urls.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.contrib.sitemaps.views import sitemap 
+from .views import BlogSitemap
+from django.conf.urls import url
+
+sitemaps = { 
+    "blog":BlogSitemap(),
+}
+
+
 app_name = "ruyatabiri"
 
 urlpatterns = [
     path('ruyatabiridetay/<int:id>',views.ruyatabiridetay, name="ruyatabiridetay"),
-    path('ruyatabiri/<char>/', views.ruya_tabiri_liste, name="ruya_tabiri_liste" ),
+    path('<char>/', views.ruya_tabiri_liste, name="ruya_tabiri_liste" ),
+
+    url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}),
 ] 
